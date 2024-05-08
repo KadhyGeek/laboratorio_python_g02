77db7d880e2519dafdd28da8f21633a922960eda
“KadhyGeekcleacleargit config --global user.email “monica1448@gmail.com <monica1448@gmail.comgit config --global user.name KadhyGeek>
2024-05-07 21:24:40 -0500

    comentario

--- Sesión 04/test_01.py
+++ Sesión 04/test_01.py
@@ -4,10 +4,7 @@
 
 nombre = input("Ingrese su nombre: ")
 apellido = input("Ingrese su apellido: ")
-
 nombre_completo = "El nombre completo del usuario es: " + nombre + " " + apellido
 print(nombre_completo)
-
-
 nombre_completo_2 = "El nombre completo del usuario es: {} {}".format(nombre, apellido)
 print(nombre_completo_2)
--- Sesión 04/test_04.py
+++ Sesión 04/test_04.py
@@ -1,12 +1,9 @@
 """Usando las propiedades de cadenas o string"""
 
 """Replace"""
-
 cadena = "Conexión a Base de Datos con Python"
-
 cadena_2 = cadena.replace(cadena[0:6], "pppppppp")
 print("Cadena con reemplazo, tiene el siguiente valor actualizado: {}".format(cadena_2))
-
 """Eliminación de espacion en blaco de mi cadena (después)"""
 cadena_3 = "Conexión a Base de Datos con Python           "
 cadena_4 = cadena_3.rstrip()
