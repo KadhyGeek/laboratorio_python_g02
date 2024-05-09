851016ffd5519ab97af4ae00868bf1b960e34c4c
“KadhyGeekcleacleargit config --global user.email “monica1448@gmail.com <monica1448@gmail.comgit config --global user.name KadhyGeek>
2024-05-09 17:48:29 -0500

    tarea 05

--- Reforzamiento 04/Refo_Try_12.py
+++ Reforzamiento 04/Refo_Try_12.py
@@ -6,5 +6,24 @@ un mensaje diciendo “Hasta luego”.
 La función a decorar pedirá el nombre de una persona y retornará el
 mensaje “Soy ‘nombre’”."""
 
+def saludar(funcion_parametro):
+    def saludo():
+        print("Buenos días")
+        funcion_parametro()
+        print("Hasta Luego")
+    return saludo()
+
+
+@saludar
+def saludo():
+    nombre = input("Ingrese el nombre del persona que desea saludar: ")
+    print("Soy "+nombre)
+
+
+
+
+
+
+
 
 
